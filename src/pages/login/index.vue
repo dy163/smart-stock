b265717149c6ca5031d0ef69a2707d0fad5f0e47
杜源 <duyuan9191@163.com>
2020-04-26 15:25:39 +0800

    *背景视频0.03*

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,30 +1,29 @@
 <template>
-  <div class="login" :style="back">
-  <!-- <div class="login"> -->
+  <!-- <div class="login" :style="back"> -->
+  <div class="login">
     <div class="login-content">
       <el-form ref="form" :model="form" label-width="60px">
-        <el-form-item label="账户">
+        <el-form-item label="账户:">
           <el-input v-model="form.account"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
+        <el-form-item label="密码:">
           <el-input v-model="form.password" type="password"></el-input>
         </el-form-item>
         <el-form-item class="login-btn">
           <el-button type="primary" @click="handleSubmit">登录</el-button>
         </el-form-item>
       </el-form>
-      <!-- 背景视频 -->
-      <!-- <div class="video-container">
-        <div :style="fixStyle" class="filter"></div>
-        <video :style="fixStyle" autoplay loop class="fillWidth" v-on:canplay="canplay" muted>
-          <source :src="PATH_TO_MP4" type="video/mp4" />浏览器不支持 video 标签，建议升级浏览器。
-        </video>
-        <div class="poster hidden" v-if="!vedioCanPlay">
-          <img :style="fixStyle" :src="PATH_TO_JPEG" alt="背景" />
-        </div>
-      </div> -->
     </div>
-    <!-- <img src="@/assets/back-img/bj.gif" alt="" class="back"> -->
+    <!-- 背景视频 -->
+    <video
+      class="video-container"
+      loop
+      src="@/assets/back-img/move.mp4"
+      autoplay="autoplay"
+      muted="muted"
+      width="100%"
+      height="100%"
+    ></video>
   </div>
 </template>
 
@@ -69,7 +68,7 @@ export default {
               type: "warning",
               message: "请正确填写账号或密码"
             });
-          } 
+          }
           if (res.data.login) {
             this.$message({
               type: "success",
@@ -95,18 +94,21 @@ export default {
 <style lang='less' scoped>
 .login {
   height: 100%;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 }
 .login-content {
   width: 500px;
-  border: 1px solid #000;
+  // border: 1px solid #000;
   border-radius: 10px;
   padding: 30px 20px 30px 0;
+  z-index: 9999;
   .el-form-item {
     /deep/.el-form-item__label {
-      color: #000;
+      color: #fff;
     }
   }
   .login-btn {
@@ -116,8 +118,8 @@ export default {
     }
   }
 }
-.back {
-  width: 100%;
-  height: 100%;
+.video-container {
+  object-fit: cover;
+  position: absolute;
 }
 </style>
