6256cad6e43a35e3ffac203960fbe944e9cbade2
yuan <duyuan9191@163.com>
2020-04-28 20:21:41 +0800

    买卖方向颜色得区别

--- src/pages/entrust/index.vue
+++ src/pages/entrust/index.vue
@@ -20,6 +20,11 @@
         <el-table-column prop="insert_time" label="委托时间"></el-table-column>
         <el-table-column prop="trade_amount" label="成交金额"></el-table-column>
         <el-table-column prop="order_status" label="状态"></el-table-column>
+        <el-table-column prop="buy_or_sell" label="方向">
+          <template slot-scope="scope">
+            <P :style="{ color: scope.row.buy_or_sell == '卖出'?  'blue' : 'red'}"> {{ scope.row.buy_or_sell }}</P>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 分页 -->
       <div class="screening-pagination">
